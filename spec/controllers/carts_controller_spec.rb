2e694df425e0707357d427f157e148108381ac09
Gianluca Maccari <brivad@gmail.com>
2010-11-24 17:19:12 +0100

    basic cart cleanup

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe CartsController do
   
-  describe "get 'reset'" do
-
-
-
-      before(:each) do
-        @cart = Factory(:cart)
-      end
-
-      it "should create a line item" do
-        lambda do
-          post :create, :product_id => @product
-        end.should change(LineItem, :count).by(1)
-      end
-
-      it "should redirect to the current cart show page" do
-        post :create, :product_id => @product
-        response.should redirect_to(current_cart_url)
-      end    
-    end
-
-  
 end

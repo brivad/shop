9b4df7ebf57e773992302c6364c5a166c774fbf1
Gianluca Maccari <brivad@gmail.com>
2010-11-25 19:19:13 +0100

    basic paypal integration

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -20,4 +20,27 @@ class Cart < ActiveRecord::Base
     line_items.to_a.sum(&:full_price)
   end
   
+  
+  def paypal_url(return_url,shipping_price)  
+    values = {  
+      :business => "seller_1290453557_biz@gmail.com",  
+      :cmd => "_cart",  
+      :upload => 1,  
+      :return => return_url,  
+      :invoice => id,
+      :currency_code => "EUR",
+      :handling_cart => shipping_price
+    }  
+
+    line_items.each_with_index do |item, index|  
+      values.merge!({  
+        "amount_#{index + 1}" => item.unit_price,  
+        "item_name_#{index + 1}" => item.product.name,  
+        "item_number_#{index + 1}" => item.product.id,  
+        "quantity_#{index + 1}" => item.quantity  
+      })  
+    end  
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.map { |k,v| "#{k}=#{v}"  }.join("&")  
+  end
+  
 end
